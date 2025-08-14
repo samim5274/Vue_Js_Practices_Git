4590883601270d3273c301b701cbead3fb8a6ecf
samim5274 <banglarcele1122@gmail.com>
2025-08-14 17:57:53 +0600

    second commit of vue practices

---
+++ Project CLI vue/vue-project/src/ContactDetials.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Contact Details</h1>
+    <p><strong>Name:</strong> {{ name }}</p>
+    <p><strong>Website:</strong> www.riad.com</p>
+    <p><strong>Address:</strong> Savar, Dhaka</p>
+    <p><strong>Phone:</strong> 01762174652</p>
+</template>
+
+<script>
+  export default {
+    data(){
+        return {
+            name: "Mamnur Rahman"
+        }
+    }
+  }
+</script>
---
+++ Project CLI vue/vue-project/src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import ContactDetials from './ContactDetials.vue';
+
+// createApp(App).mount('#app')
+
+
+console.log(App);
+
+var app = createApp(App);
+
+app.component('contact-details', ContactDetials);
+
+app.mount('#app');
